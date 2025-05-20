72bd7c469c03198ccf4d8eb89a0d2219e27a03e5
Color Hut Official <colorhut.official@gmail.com>
2025-05-20 10:14:37 +0000

    changr trxt color for It seems the page you are looking for has wandered off into the digital wilderness. Don't worry, we can guide you back.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -36,7 +36,7 @@ export default function NotFound() {
       </motion.p>
 
       <motion.p
-        className="text-md sm:text-lg text-muted-foreground mb-10 max-w-md"
+        className="text-md sm:text-lg text-gray-300 mb-10 max-w-md"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.6 }}
