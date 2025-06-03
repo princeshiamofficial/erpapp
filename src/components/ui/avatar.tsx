2119c84c41eb0bd86e7269d591880f266fbe8a47
Color Hut Official <colorhut.official@gmail.com>
2025-06-03 09:10:08 +0000

    still showing same

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -27,7 +27,7 @@ const AvatarImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full object-cover rounded-full", className)} // Re-added rounded-full
+    className={cn("h-full w-full object-cover rounded-full", className)} // Removed aspect-square
     {...props}
   />
 ))
