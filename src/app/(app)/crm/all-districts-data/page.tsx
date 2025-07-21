3269cae94ebfb2d9e40e5f1d6603250a6e525a21
Color Hut Official <colorhut.official@gmail.com>
2025-07-21 05:09:10 +0000

    make more beautiful and eye-catching design All Districts Data page

--- src/app/(app)/crm/all-districts-data/page.tsx
+++ src/app/(app)/crm/all-districts-data/page.tsx
@@ -3,7 +3,6 @@
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { Input } from '@/components/ui/input';
 
 const districtsData = [
   {
