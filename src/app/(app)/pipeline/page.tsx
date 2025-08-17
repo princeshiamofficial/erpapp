5251be79e85f317ed14cf22338907dc57b256b05
Color Hut Official <colorhut.official@gmail.com>
2025-08-17 12:48:29 +0000

    remove real-time feel & auto refresh on pipeline page ensure don't chang

--- src/app/(app)/pipeline/page.tsx
+++ src/app/(app)/pipeline/page.tsx
@@ -112,8 +112,11 @@ export default function PipeLinePage() {
   }, [toast, currentUser]);
 
   useEffect(() => {
-    fetchLeadsAndUsers();
-  }, [fetchLeadsAndUsers]);
+    if (currentUser) {
+      fetchLeadsAndUsers();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentUser]);
 
   const filteredLeads = useMemo(() => {
     let baseLeads = leads;
