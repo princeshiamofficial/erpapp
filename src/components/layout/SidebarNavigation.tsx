62ab7d24434ebd5245d2c5d8a934783452c8541e
Color Hut Official <colorhut.official@gmail.com>
2025-08-28 12:10:15 +0000

    add crm sub menu AdEx when click the button then redirect  to colorhutbd

--- src/components/layout/SidebarNavigation.tsx
+++ src/components/layout/SidebarNavigation.tsx
@@ -58,6 +58,7 @@ const navItems: NavItem[] = [
       { href: "/pipeline", label: "Pipe Line", icon: Briefcase, roles: ["SYSTEM_ADMIN", "ADMIN", "CRM"] },
       { href: "/crm/all-districts-data", label: "ADD", icon: Map, roles: ["SYSTEM_ADMIN", "ADMIN", "CRM"] },
       { href: "/crm/sow", label: "SOW", icon: FolderHeart, roles: ["SYSTEM_ADMIN", "ADMIN", "CRM"] },
+      { href: "https://colorhutbd.xyz/info", label: "AdEx", icon: Printer, roles: ["SYSTEM_ADMIN", "ADMIN", "CRM"] },
     ]
   },
   { href: "/orders", label: "Orders", icon: Package, roles: ["SYSTEM_ADMIN", "ADMIN", "CRM"] },
