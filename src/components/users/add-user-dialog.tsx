0a8c7094e05b1f4a92e84989b79c2a27a1112502
Color Hut Official <colorhut.official@gmail.com>
2025-09-15 08:13:30 +0000

    ReferenceError: Textarea is not defined     at AddUserDialog (https://60

--- src/components/users/add-user-dialog.tsx
+++ src/components/users/add-user-dialog.tsx
@@ -7,6 +7,7 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from '@/components/ui/textarea';
 import type { User, UserRole } from "@/types";
 import { useToast } from '@/hooks/use-toast';
 import Image from 'next/image';
