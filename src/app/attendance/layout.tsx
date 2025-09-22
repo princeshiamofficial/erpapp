c3eea69292ee0174a30d670d802ea5a487e7b997
Color Hut Official <colorhut.official@gmail.com>
2025-09-22 12:40:06 +0000

    disable zoom-in/zoom-out for /attendance & /attendance/[path] route. ens

--- src/app/attendance/layout.tsx
+++ src/app/attendance/layout.tsx
@@ -1,8 +1,17 @@
 
 import { BottomNavigation } from '@/components/layout/BottomNavigation';
+import type { Viewport } from 'next';
 import React from 'react';
 
 // This new layout will wrap all pages inside the /attendance route group
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function AttendanceLayout({
   children,
 }: {
