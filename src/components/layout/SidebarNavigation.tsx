29397b5dbc14cf4cbe8bc924f326425c3b9e918f
Color Hut Official <colorhut.official@gmail.com>
2025-09-23 06:45:50 +0000

    move the existing "User Management", "Service Options" and "Model Manage

--- src/components/layout/SidebarNavigation.tsx
+++ src/components/layout/SidebarNavigation.tsx
@@ -80,6 +80,9 @@ const navItems: NavItem[] = [
     subItems: [
       { href: "/vendors", label: "Vendors", icon: Store, roles: ["SYSTEM_ADMIN", "ADMIN"] },
       { href: "/payroll", label: "Payroll", icon: Landmark, roles: ["SYSTEM_ADMIN", "ADMIN"] },
+      { href: "/users", label: "User Management", icon: Users, roles: ["SYSTEM_ADMIN", "ADMIN"] },
+      { href: "/admin/service-management", label: "Service Options", icon: Settings2, roles: ["SYSTEM_ADMIN"] },
+      { href: "/admin/model-management", label: "Model Management", icon: Layers, roles: ["SYSTEM_ADMIN", "ADMIN"] },
     ]
   },
   { href: "/finance-manager", label: "Finance Manager", icon: DollarSign, roles: ["SYSTEM_ADMIN", "ADMIN", "CRM", "DESIGNER_REPRESENTATIVE", "LR"] },
@@ -95,11 +98,8 @@ const navItems: NavItem[] = [
     roles: ["SYSTEM_ADMIN", "ADMIN"], 
     href: "",
     subItems: [
-      { href: "/users", label: "User Management", icon: Users, roles: ["SYSTEM_ADMIN", "ADMIN"] },
       { href: "/admin/custom-access", label: "Custom Access", icon: Shield, roles: ["SYSTEM_ADMIN"] },
-      { href: "/admin/model-management", label: "Model Management", icon: Layers, roles: ["SYSTEM_ADMIN", "ADMIN"] },
       { href: "/admin/statuses", label: "Status Management", icon: ListChecks, roles: ["SYSTEM_ADMIN"] },
-      { href: "/admin/service-management", label: "Service Options", icon: Settings2, roles: ["SYSTEM_ADMIN"] },
       { href: "/admin/crm-target-settings", label: "App Settings", icon: Target, roles: ["SYSTEM_ADMIN"] }, 
       { href: "/admin/attendance", label: "Attendance", icon: Users, roles: ["SYSTEM_ADMIN", "ADMIN"] },
     ]
