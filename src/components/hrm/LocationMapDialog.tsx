786a6a605cd3f99788afd5720bc78777c2b28113
Color Hut Official <colorhut.official@gmail.com>
2025-09-24 09:53:50 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Map container is

--- src/components/hrm/LocationMapDialog.tsx
+++ src/components/hrm/LocationMapDialog.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/dialog";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'; // Corrected import
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet-defaulticon-compatibility';
 
 interface LocationMapDialogProps {
