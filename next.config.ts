9e69621ba2d082ed6b7028d31ef54616209cae3d
Color Hut Official <colorhut.official@gmail.com>
2025-09-26 04:07:23 +0000

    Failed to load resource: the server responded with a status of 403 ()

--- next.config.ts
+++ next.config.ts
@@ -44,6 +44,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async headers() {
