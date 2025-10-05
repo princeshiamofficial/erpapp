5958eaf8da1f10496918ab54d021e8d511bda681
Color Hut Official <colorhut.official@gmail.com>
2025-10-05 10:59:02 +0000

    fix the error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning style={GeistSans.style}>
-      <body className='font-sans antialiased'>
+      <body className='font-sans antialiased' suppressHydrationWarning>
         <Providers>
           {children}
         </Providers>
