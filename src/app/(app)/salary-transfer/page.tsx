390c19393f2d736f65c2a5ae6b74fafec094b16e
Color Hut Official <colorhut.official@gmail.com>
2025-10-09 05:44:03 +0000

    replce to House No. 14, Road No. A, Block A, Sontek Area, South Kajla, J

--- src/app/(app)/salary-transfer/page.tsx
+++ src/app/(app)/salary-transfer/page.tsx
@@ -79,7 +79,7 @@ export default function SalaryTransferPage() {
           <div className="flex justify-center">
              <Image src="https://i.ibb.co/FFQMvkz/logo-02-01.jpg" alt="Company Logo" width={160} height={40} className="object-contain rounded-md" />
           </div>
-          <CardDescription className="text-red-500">Address</CardDescription>
+          <CardDescription className="text-red-500">House No. 14, Road No. A, Block A, Sontek Area, South Kajla, Jatrabari, Dhaka - 1236</CardDescription>
         </CardHeader>
         <CardContent className="px-2 sm:px-6">
            <div className="border-y border-gray-300 py-2 my-4">
