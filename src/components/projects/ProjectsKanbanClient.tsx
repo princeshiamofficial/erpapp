8d4c39d3b2847757ad8db2559afde0dc5f1845f7
Color Hut Official <colorhut.official@gmail.com>
2025-12-01 06:59:35 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Loader2 is not d

--- src/components/projects/ProjectsKanbanClient.tsx
+++ src/components/projects/ProjectsKanbanClient.tsx
@@ -16,7 +16,8 @@ import {
   ClipboardList,
   Search,
   EyeOff,
-  Download // New Icon
+  Download, // New Icon
+  Loader2
 } from 'lucide-react'; 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
