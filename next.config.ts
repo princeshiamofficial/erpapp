9a339396d8754cfb4056d9bb43a38632c9e70660
Prince Shiam <prince.shiam.official@gmail.com>
2026-05-09 14:30:10 +0600

    fix(build): add @react-pdf/renderer to transpilePackages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['@react-pdf/renderer'],
   poweredByHeader: false,
   compress: true,
   images: {
